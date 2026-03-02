b0b855bd42080f6d8401c237812cfa25b7250a11
mikecalderon742-cell <mikecalderon742@gmail.com>
2026-03-01 18:11:09 -0800

    Fix 500 error and fix invoice card

--- app.py
+++ app.py
@@ -2845,6 +2845,15 @@ def health():
     return "OK", 200
 
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(
+        os.path.join(app.root_path, 'static'),
+        'favicon.ico',
+        mimetype='image/vnd.microsoft.icon'
+    )
+
+
 # -------------------------
 # MAIN
 # -------------------------
